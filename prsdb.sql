69ddad0dd11dba0a6d4f760336e631ba284844c5
jimpatton <jimpatton5150@gmail.com>
2025-01-30 16:30:50 -0500

    init commit

---
+++ prsdb.sql
@@ -0,0 +1,57 @@
+USE master
+GO
+
+DROP DATABASE IF EXISTS prsdb
+	
+CREATE DATABASE prsdb
+GO
+
+USE prsdb
+GO
+CREATE TABLE [User](
+	ID				INT				IDENTITY(1,1)	PRIMARY KEY,
+	Username		VARCHAR(20)		NOT NULL,
+	Password		VARCHAR(10)		NOT NUll,
+	FirstName		VARCHAR(20)		NOT NULL,
+	LastName		VARCHAR(20)		NOT NULL,
+	PhoneNumber		VARCHAR(12)		NOT NULL,
+	Email			VARCHAR(75)		NOT NULL,
+	Reviewer		BIT				NOT NULL,
+	Admin			BIT				NOT NULL,
+	CONSTRAINT uname UNIQUE (Username)
+	);
+	GO
+
+CREATE TABLE Vendor(
+	ID				INT				NOT NULL		PRIMARY KEY,
+	Code			VARCHAR(10)		NOT NULL,
+	Name			VARCHAR(255)	NOT NULL,
+	Address			VARCHAR(255)	NOT NULL,
+	City			VARCHAR(255)	NOT NULL,
+	State			VARCHAR(255)	NOT NULL,
+	Zip				VARCHAR(5)		NOT NULL,
+	PhoneNumber		VARCHAR(12)		NOT NULL,
+	Email			VARCHAR(100)	NOT NULL,
+	CONSTRAINT	vcode UNIQUE (Code)
+	);
+	GO
+
+CREATE TABLE [Product](
+	ID				INT				NOT NULL		PRIMARY KEY,
+	VendorID		INT				NOT NULL,
+	PartNumber		VARCHAR(50)		NOT NULL,
+	Name			VARCHAR(150)	NOT NULL,
+	Price			DECIMAL(10,2)	NOT NULL,	
+	Unit			VARCHAR(255),
+	Photopath		VARCHAR(255),
+	CONSTRAINT vendor_part UNIQUE (VendorID, PartNumber),
+	CONSTRAINT FK_ProductVendor FOREIGN KEY (VendorID) REFERENCES Vendor(ID)
+	)
+	GO
+
+CREATE TABLE Request(
+	ID				INT				NOT NULL		PRIMARY KEY,
+	UserID			INT				NOT NULL,
+	RequestNumber	VARCHAR(20)		NOT NULL,
+	Description		VARCHAR(100)	NOT NULL,
+	Justification	VARCHAR(255)	NOT NULL,
